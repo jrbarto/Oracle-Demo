807a1a2ad97318326a7bbe69d46f93caafb69d9b
Jeffrey Barto <bartorobjeff@gmail.com>
2018-09-19 18:19:53 -0400

    Added a create_procedure file.

---
+++ sql_scripts/create_procedure.sql
@@ -0,0 +1,8 @@
+CREATE PROCEDURE remove_emp (employee_id NUMBER) AS
+    total_employees NUMBER;
+    BEGIN
+        DELETE FROM employees
+        WHERE employees.employee_id = remove_emp.employee_id;
+    total_employees := total_employees - 1;
+    END;
+/
