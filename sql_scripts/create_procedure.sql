2a2c1178d4de658c0e5bea0b9417f05dffa223eb
Jeffrey Barto <bartorobjeff@gmail.com>
2018-09-19 18:25:49 -0400

    Added error to create_procedure.sql.

--- sql_scripts/create_procedure.sql
+++ sql_scripts/create_procedure.sql
@@ -1,7 +1,7 @@
 CREATE PROCEDURE remove_emp (employee_id NUMBER) AS
     total_employees NUMBER;
     BEGIN
-        DELETE FROM employees
+        DELETE FROP employees
         WHERE employees.employee_id = remove_emp.employee_id;
     total_employees := total_employees - 1;
     END;
