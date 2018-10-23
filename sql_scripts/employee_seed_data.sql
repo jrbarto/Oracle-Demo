0b81ac60378aa2fce99f4e4197e8bf2a85a0638a
Jeffrey Barto <bartorobjeff@gmail.com>
2018-10-23 11:28:58 -0400

    Added two 2994 characters to data.

--- sql_scripts/employee_seed_data.sql
+++ sql_scripts/employee_seed_data.sql
@@ -11,4 +11,4 @@ VALUES
 INSERT INTO employees                                                            
 (employee_id, first_name, last_name, city, state, phone, address)                
 VALUES                                                                           
-(3, 'Sylvain', 'Carbonell', 'Cleveland', 'OH', '555-5555', '8221 Euclid Ave.'); 
+(3, 'Sylvain', '$$Carbonell', 'Cleveland', 'OH', '555-5555', '8221 Euclid Ave.'); 
