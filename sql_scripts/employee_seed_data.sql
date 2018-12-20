21d7faaa85d0d4af2fa3aee088bc74bbbd55b634
Jeffrey Barto <bartorobjeff@gmail.com>
2018-12-20 11:31:07 -0500

    Test concatenation.

--- sql_scripts/employee_seed_data.sql
+++ sql_scripts/employee_seed_data.sql
@@ -1,4 +1,4 @@
-INSERT INTO employees (employee_id, first_name, last_name, city, state, phone, address) VALUES (1, '--Jeff', 'Barto', 'Cleveland', 'OH', '555-5555', '8221 Euclid Ave.')
+INSERT INTO employees (employee_id, first_name, last_name, city, state, phone, address) VALUES (1, CONCAT(char(45), 'Jeff'), 'Barto', 'Cleveland', 'OH', '555-5555', '8221 Euclid Ave.')
 ;
 INSERT INTO employees (employee_id, first_name, last_name, city, state, phone, address) VALUES (2, 'Mike', 'Samano', 'Cleveland', 'OH', '555-5555', '8221 Euclid Ave.')
 ;
