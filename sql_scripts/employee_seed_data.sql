a0296e0a99a5026783ef6696fb5d4aaf12e275ac
Jeffrey Barto <bartorobjeff@gmail.com>
2019-04-08 15:25:31 -0400

    Updated

--- sql_scripts/employee_seed_data.sql
+++ sql_scripts/employee_seed_data.sql
@@ -1,3 +1,3 @@
-INSERT INTO employees (employee_id, first_name, last_name, city, state, phone, address) VALUES (1, '--Jeff', 'Barto', 'Cleveland', 'OH', '555-5555', '8221 Euclid Ave.');
+INSERT INTO employees (employee_id, first_name, last_name, city, state, phone, address) VALUES (1, 'Bancóldex', 'Barto', 'Cleveland', 'OH', '555-5555', '8221 Euclid Ave.');
 INSERT INTO employees (employee_id, first_name, last_name, city, state, phone, address) VALUES (2, 'Mike', 'Samano', 'Cleveland', 'OH', '555-5555', '8221 Euclid Ave.');
 INSERT INTO employees (employee_id, first_name, last_name, city, state, phone, address) VALUES (3, 'Sylvain', 'Carbonell', 'Cleveland', 'OH', '555-5555', '8221 Euclid Ave.');
